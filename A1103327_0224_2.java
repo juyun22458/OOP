9ee4883037affb074147de0ff5f456a12a9d5479
juyun22458 <125621559+juyun22458@users.noreply.github.com>
2023-03-01 13:21:45 +0800

    Update A1103327_0224_2.java

--- A1103327_0224_2.java
+++ A1103327_0224_2.java
@@ -5,7 +5,7 @@ public class A1103327_0224_2{
         System.out.println("請輸入攝氏溫度:");
         Scanner input = new Scanner(System.in);
         int a = input.nextInt();
-        double b = ( a * 9.0/5 )+ 32;
-        System.out.println("換算為華氏溫度:"+b);
+        double b = ( a * 9.0 / 5 )+ 32;
+        System.out.println("換算成華氏溫度:"+b);
     }
 }
